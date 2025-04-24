a95c23acec73f0fa1f9fa345a2147fe8a44c9f2a
mitaka1959 <dimitar.stanilov@gmail.com>
2025-04-24 15:15:05 +0300

    Refreshing Tokens

--- EasyStays.Application/Interfaces/Auth/IJwtProvider.cs
+++ EasyStays.Application/Interfaces/Auth/IJwtProvider.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +12,7 @@ namespace EasyStays.Application.Interfaces.Auth
 {
     public interface IJwtProvider
     {
-        string Generate(string userId, string userName, string email, string role);
+        string Generate(IEnumerable<Claim> claims);
     }
 
 }
--- EasyStays.Infrastructure/Auth/JwtProvider.cs
+++ EasyStays.Infrastructure/Auth/JwtProvider.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using EasyStays.Application.Interfaces.Auth;
+using EasyStays.Domain.Entities;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 
@@ -18,26 +20,39 @@ namespace EasyStays.Infrastructure.Auth
         {
             _configuration = configuration; 
         }
-        public string Generate(string userId, string userName, string email, string role)
+
+        public (string AccessToken, string RefreshToken) GenerateTokens(string userId, string userName, string email, string role)
         {
-            var Claims = new[]
+            var claims = new List<Claim>
             {
                 new Claim(JwtRegisteredClaimNames.Sub, userId),
                 new Claim(JwtRegisteredClaimNames.UniqueName, userName),
                 new Claim(JwtRegisteredClaimNames.Email, email),
                 new Claim(ClaimTypes.Role, role)
             };
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
-            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var accessToken = Generate(claims);
+            var refreshToken = GenerateRefreshToken();
 
+            return (accessToken, refreshToken);
+        }
+        public string Generate(IEnumerable<Claim> claims)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:SecretKey"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
             var token = new JwtSecurityToken(
                 issuer: _configuration["Jwt:Issuer"],
                 audience: _configuration["Jwt:Audience"],
-                claims: Claims,
-                expires: DateTime.UtcNow.AddHours(1),
-                signingCredentials: credentials
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(15), 
+                signingCredentials: creds
             );
+
             return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private string GenerateRefreshToken()
+        {
+            return Guid.NewGuid().ToString(); 
         }
     }
 }
