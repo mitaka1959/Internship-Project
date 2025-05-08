299b634360a019abf46536e67b56e5699b2ddbd0
mitaka1959 <dimitar.stanilov@gmail.com>
2025-05-08 15:48:48 +0300

    DashboardHeader

--- FrontEnd/easy-stays-frontend/src/features/auth/pages/host/dashboard/DashboardHeader.tsx
+++ FrontEnd/easy-stays-frontend/src/features/auth/pages/host/dashboard/DashboardHeader.tsx
@@ -1,4 +1,25 @@
-const DashboardHeader = () => (
-  <h2 style={{ marginBottom: "1rem" }}>Welcome back, Host!</h2>
-);
+import React from "react";
+import { Typography } from "antd";
+
+const { Title } = Typography;
+
+const DashboardHeader: React.FC = () => {
+  const userName = localStorage.getItem("userName");
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: "2rem",
+      }}
+    >
+      <Title level={3} style={{ margin: 0, color: "#023047" }}>
+        Welcome back, {userName}!
+      </Title>
+    </div>
+  );
+};
+
 export default DashboardHeader;
